98b2f0a49e2ee337f1de1a2e69f00ffc5b8ca1d3
xtzeng <xiaoti_zeng@126.com>
2014-06-30 07:09:14 +0800

    add sth format

--- src/main/java/com/xiaoti/model/Teacher.java
+++ src/main/java/com/xiaoti/model/Teacher.java
@@ -22,12 +22,14 @@ public class Teacher {
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getTitle() {
 		return title;
 	}
